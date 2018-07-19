d37eac597e24a433750b9072ec8f145473b57c2a
kagedani <ubo1995@gmail.com>
2018-07-19 16:54:04 +0200

    search for params (all sensors)

--- loop_parameters.py
+++ loop_parameters.py
@@ -1,3 +1,14 @@
+'''
+>>> 17-07
+i = 23
+o = 23
+
+>>> 19-07
+- ora: 14
+i = 21 (+/-2)
+o = 17 (+/-2)
+'''
+
 import just_processing as jp
 from functions import parse_args
 from sklearn.metrics import mean_squared_error
@@ -13,13 +24,7 @@ import getopt
 REAL_IN = 23
 REAL_OUT = 23
 actual_values = [REAL_IN, REAL_OUT]
-span = [600,1000]
-infrared_param = [4, 5, 7, 10, 12]
-enough_zero = [10, 15, 20, 25, 30]
-delta = [1400, 2000]
 
-span_jump = 50
-delta_jump = 100
 
 ground_truth_date = "17_07"
 
@@ -34,6 +39,17 @@ results = []
 
 use = parse_args()
 
+if use[0] == True:
+	var = [600,1000]
+	delta = [1400, 2000]
+	var_jump = 50
+	delta_jump = 100
+
+elif use[1] == True:
+	var = [10, 25]
+	delta = [1400, 2000]
+	var_jump = 1
+	delta_jump = 100
 
 
 with open(DATA_INPUT_A) as side_a:
@@ -44,16 +60,17 @@ with open(DATA_INPUT_B) as side_b:
 en = 0
 ex = 0
 
-for s in range(span[0], span[1]+span_jump, span_jump):
+
+for v in range(span[0], var[1]+var_jump, var_jump):
 	for d in range(delta[0], delta[1]+delta_jump, delta_jump):
-		en, ex = jp.just_processing(a, b, d, s, None, None, use)
+		en, ex = jp.just_processing(a, b, d, v, use)
 		temp = [] 
 		temp.append(en)
 		temp.append(ex)
 		pred = [en, ex]
 		temp.append("%.2f" % sqrt(mean_squared_error(actual_values, pred)))
 		temp.append("%.2f" % mean_absolute_error(actual_values, pred))
-		temp.append(s)
+		temp.append(v)
 		temp.append(d)
 		results.append(temp)
 		with open(OUTPUT_PATH_PARTIAL, 'a') as partial:
@@ -61,7 +78,11 @@ for s in range(span[0], span[1]+span_jump, span_jump):
 			writer.writerow(temp) 
 		
 
-results_pd = pd.DataFrame(results, columns=['IN', 'OUT', 'RMSE', 'MAE', 'span', 'delta'])
-print("Ho finito :)")
-results_pd.to_csv(OUTPUT_PATH, sep='\t')
-
+if use[0]:
+	results_pd = pd.DataFrame(results, columns=['IN', 'OUT', 'RMSE', 'MAE', 'span', 'delta'])
+	print("Ho finito :)")
+	results_pd.to_csv(OUTPUT_PATH, sep='\t')
+elif use[1]:
+	results_pd = pd.DataFrame(results, columns=['IN', 'OUT', 'RMSE', 'MAE', 'enough_zero', 'delta'])
+	print("Ho finito :)")
+	results_pd.to_csv(OUTPUT_PATH, sep='\t')
