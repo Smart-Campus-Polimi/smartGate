19ccc49480e8e10dec0f686575d69a6474e9f01a
Andrea Ganassa <wunagana@MacBook-Pro-di-Andrea-3.local>
2018-10-01 15:57:54 +0200

    Compact Version Multiple ToF

---
+++ loop_parameters_multiple_tof.py
@@ -0,0 +1,70 @@
+
+import jp_graph_trial_multiple_tof as jp
+from functions import parse_args
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from math import sqrt
+import json
+import pandas as pd
+import time
+import csv
+from pathlib import Path
+
+ground_truth_date = "01_10"
+ground_truth_time = "14_56"
+
+output_date = "27_09"
+CM = "normal_<_"
+
+#PATH = "/home/cluster/smartGate/"
+#PATH = "/home/daniubo/Scrivania/smartGate/"
+PATH = "/Users/wunagana/Documents/GitHub/smartGate/"
+#DATA_INPUT_A = PATH + "ground_truth_realistic/side_a_"+ground_truth_date+"_part6.json"
+#DATA_INPUT_B = PATH + "ground_truth_realistic/side_b_"+ground_truth_date+"_part6.json"
+DATA_INPUT = PATH + "ground_truth_realistic/"+ ground_truth_date +"/multiple_tof_"+ground_truth_time+".json"
+
+
+results = []
+
+with open(DATA_INPUT) as side_a:
+	a = json.load(side_a)
+with open(DATA_INPUT) as side_b:
+	b = json.load(side_b)
+
+en = 0
+ex = 0
+
+
+
+en, ex, real_en, real_ex = jp.just_processing(a, b, ground_truth_time)
+
+actual_values = [real_en, real_ex]
+temp = []
+temp.append(ground_truth_time)
+temp.append(real_en)
+temp.append(en)
+temp.append(real_ex)
+temp.append(ex)
+pred = [en, ex]
+temp.append("%.2f" % sqrt(mean_squared_error(actual_values, pred)))
+temp.append("%.2f" % mean_absolute_error(actual_values, pred))
+if actual_values[0] != 0 and actual_values[1] != 0:
+	acc_in = 100-(abs(en-actual_values[0])/actual_values[0] * 100)
+	acc_out = 100-(abs(ex-actual_values[1])/actual_values[1] * 100)
+	temp.append(acc_in)
+	temp.append(acc_out)
+else:
+	temp.append('NaN')
+	temp.append('NaN')
+
+results.append(temp)
+OUTPUT_PATH = PATH+"output/"+output_date+"/"+output_date+CM+"_tof_results.csv"
+file = Path(OUTPUT_PATH)
+if file.is_file():
+	with open(OUTPUT_PATH, 'a') as partial:
+		writer = csv.writer(partial, delimiter=',')
+		writer.writerow(results)
+else:
+	results_pd = pd.DataFrame(results, columns=['TIME', 'REAL IN', 'IN', 'REAL OUT', 'OUT', 'RMSE', 'MAE', 'ACC. IN', 'ACC.OUT'])
+	print("Ho finito :)")
+	results_pd.to_csv(OUTPUT_PATH, sep=',')
