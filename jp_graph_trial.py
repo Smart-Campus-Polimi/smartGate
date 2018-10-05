b7362ee7e976d9d764fd58649ceb75413adc8bfc
kagedani <ubo1995@gmail.com>
2018-10-05 15:30:02 +0200

    unified program

--- jp_graph_trial.py
+++ jp_graph_trial.py
@@ -6,23 +6,25 @@ import functions as f
 import signal
 import getopt
 import sys
+import csv
 
+DATA = "27-09-2018"
+PATH = "GT_telefono/27_09/"
+DATE =  "27_09.txt"
+FUSO_ORARIO = 7188000
+OUTPUT_PATH_INFRA_TS = "/home/daniubo/Scrivania/Git/smartGate/analysis/match_analysis/"
+OUTPUT_PATH_PIR_TS = "/home/daniubo/Scrivania/Git/smartGate/analysis/match_analysis/"
+OUTPUT_PATH_TOF_TS = "/home/daniubo/Scrivania/Git/smartGate/analysis/match_analysis/"
 
 def just_processing(a, b, delta, var, use, TIME):
 	use_infra = use[0]
 	use_pir = use[1]
 	use_camera = use[2]
 	do_graph = use[3]
+	check_matching = use[4]
+	use_tof = use[5]
 	span = var
 	enough_zero = var
-	#use_anal = use[3]
-
-	'''
-	with open('side_a.json') as side_a:
-		a = json.load(side_a)
-	with open('side_b.json') as side_b:
-		b = json.load(side_b)
-	'''
 	infrared_a = [];
 	infrared_b = [];
 	infrared_mask = [];
@@ -165,30 +167,25 @@ def just_processing(a, b, delta, var, use, TIME):
 			U_b = []
 			ins_a, out_a, E_a, U_a = f.count_entries(activation_p1a,activation_p0a,'A', activation_p1b, activation_p0b, delta, span)
 			ins_b, out_b, E_b, U_b = f.count_entries(activation_p1b,activation_p0b,'B', activation_p1a, activation_p0a, delta, span)
-
+			'''
+			if check_matching:
+				with open(OUTPUT_PATH_PIR_TS+"pir_ts.csv", 'a') as match:
+					writer = csv.writer(match, delimiter=';')
+					writer.writerow(['TS', 'TypeOfEvent'])
+					for element in E_a:
+						writer.writerow([element, 1])
+					for element in U_a:
+						writer.writerow([element, 0])
+					for element in E_b:
+						writer.writerow([element, 3])
+					for element in U_b:
+						writer.writerow([element, 2])
+			'''				
 		if processing:
 			I_pir = max(ins_a, ins_b);
 			O_pir= max(out_a, out_b);
 
 		if do_graph:
-			'''
-			#GRAPH BUILDER
-			ts_array = np.loadtxt("/home/daniubo/Scrivania/smartGate/appTS/ts_"+TIME+"_csv.csv", dtype = int, delimiter = ",", skiprows=1)
-			ts_in = []
-			ts_out = []
-			for element in ts_array:
-				if element[1] == 1:
-					ts_in.append([element[0], 1.2])
-				elif element[2] == 1:
-					ts_out.append([element[0], 1.2])
-			ts_in = f.from_ms_to_date(ts_in)
-			ts_out = f.from_ms_to_date(ts_out)
-			#print("Qui aggiungere codice per grafici")
-			'''
-			p0a[0] = f.from_ms_to_date(p0a[0])
-			p0b[0] = f.from_ms_to_date(p0b[0])
-			p1a[0] = f.from_ms_to_date(p1a[0])
-			p1b[0] = f.from_ms_to_date(p1b[0])
 			plt.plot(*zip(*p0a), color='green', label='p0a')
 			plt.plot(*zip(*p0b), color='orange', label='p0b')
 			plt.plot(*zip(*p1a), color='blue', label='p1a')
@@ -199,14 +196,6 @@ def just_processing(a, b, delta, var, use, TIME):
 			plt.plot(U_b, [1.25]*len(U_b), 'rx', color='black', label='algorithm exits B')
 			plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
 			plt.ylim(-0.2, 2)
-			'''
-			plt.plot(*zip(*ts_in), color='black', marker='o', linestyle='dashed')
-			plt.plot(*zip(*ts_out), color='red', marker='x', linestyle='dashed')
-			real_in = mlines.Line2D([],[],color='black', label='real_in')
-			real_out = mlines.Line2D([],[],color='red', label='real_out')
-			plt.legend(handles=[green_pir, orange_pir, blue_pir, red_pir, real_in, real_out])
-			'''
-			#plt.title("Real events")
 			plt.show()
 
 	if use_infra:
@@ -229,48 +218,150 @@ def just_processing(a, b, delta, var, use, TIME):
 			U = []
 			I_inf = 0
 			O_inf = 0
-			I_inf,O_inf, E, U = f.count_entries_tof(activate_infra_0, activate_infra_1, delta, I_inf, O_inf, E, U)
+			I_inf,O_inf, E_inf, U_inf = f.count_entries_tof(activate_infra_0, activate_infra_1, delta, I_inf, O_inf, E, U)
 			print(">>> Infrared:\tIN:",I_inf,"\tO:",O_inf,"\n")
+			'''
+			if check_matching:
+				with open(OUTPUT_PATH_INFRA_TS+"infrared_ts.csv", 'a') as match:
+					writer = csv.writer(match, delimiter=';')
+					writer.writerow(['TS', 'TypeOfEvent'])
+					for element in E:
+						writer.writerow([element - min_ts_infra, 1])
+					for element in U:
+						writer.writerow([element - min_ts_infra, 0])
+			'''
 		if do_graph:
-			'''
-			ts_array = np.loadtxt("/home/daniubo/Scrivania/smartGate/appTS/ts_"+TIME+"_csv.csv", dtype = int, delimiter = ",", skiprows=1)
-			ts_in = []
-			ts_out = []
-			for element in ts_array:
-				if element[1] == 1:
-					ts_in.append([element[0], 1.2])
-				elif element[2] == 1:
-					ts_out.append([element[0], 1.2])
-			ts_in = f.from_ms_to_date(ts_in)
-			ts_out = f.from_ms_to_date(ts_out)
-			'''
-			infrared_a[0] = f.from_ms_to_date(infrared_a[0])
-			infrared_b[0] = f.from_ms_to_date(infrared_b[0])
-			E = f.from_ms_to_date(E)
-			U = f.from_ms_to_date(U)
 			# INFRARED
 			plt.plot(*zip(*infrared_a), color='green', label ='inf_a')
-			#green_inf = mlines.Line2D([], [], color='green', label='inf_a')
 			plt.plot(*zip(*infrared_b), color='red', label='inf_b')
-			#red_inf = mlines.Line2D([], [], color='red', label='inf_b')
-			plt.plot(E, [1.5]*len(E), 'ro', color='blue', label='algorithm entries')
-			plt.plot(U, [1.5]*len(U), 'ro', color='black', label='algorithm exits')
+			plt.plot(E_inf, [1.5]*len(E_inf), 'ro', color='blue', label='algorithm entries INFRA')
+			plt.plot(U_inf, [1.5]*len(U_inf), 'ro', color='black', label='algorithm exits INFRA')
 			plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
-			#plt.legend(handles=[green_inf, red_inf])
-			'''
-			# REAL
-			plt.plot(*zip(*ts_in), color='black', marker='o', linestyle='dashed')
-			plt.plot(*zip(*ts_out), color='orange', marker='x', linestyle='dashed')
-			real_in = mlines.Line2D([],[],color='black', label='real_in')
-			real_out = mlines.Line2D([],[],color='orange', label='real_out')
-			plt.legend(handles=[green_inf, red_inf, real_in, real_out])
-			'''
 			ylim = (-0.2, 2)
 			#plt.title("Infrared")
 			plt.show();
 
+	if use_tof:
+		tof0 = [];
+		tof1 = [];
 
-	#print ('################################\nEffective entries: ', I,'\nEffective Exits: ', O,'\n################################')
+		tof0_temp = []
+		tof1_temp = []
+		for packet in a:
+			tof0_temp = packet['TOF']
+			number_of_samples = len(tof0_temp);
+			time_vector = f.building_time(packet, round(float(1000/number_of_samples)));
+			partial_tof0 = [list(a) for a in zip(time_vector, tof0_temp)]
+			tof0 = tof0 + partial_tof0;
+		for packet in b:
+			tof1_temp = packet['TOF']
+			number_of_samples = len(tof1_temp);
+			time_vector = f.building_time(packet, round(float(1000/number_of_samples)));
+			partial_tof1 = [list(b) for b in zip(time_vector, tof1_temp)]
+			tof1 = tof1 + partial_tof1;
+
+		tof1 = f.correcting_errors_tof(tof1)
+		tof0 = f.correcting_errors_tof(tof0)
+
+		min_ts_tof = min(tof1[0][0],tof0[0][0])
+		max_ts_tof = max(tof1[len(tof1)-1][0],tof0[len(tof0)-1][0])
+		interval = int(max_ts-min_ts)
+
+		min_ts_tof0 = tof0[0][0]
+		min_ts_tof1 = tof1[0][0]
+		max_ts_tof0 = tof0[-1][0]
+		max_ts_tof1 = tof1[-1][0]
+
+		array_support = []
+		for i in range(0,interval):
+			array_support.append(0);
+
+		processing = True
+		uniform_tof0 = f.uniform_list_tof(array_support, tof0, min_ts_tof0, max_ts_tof0, min_ts_tof, max_ts_tof)
+		if (not uniform_tof0):
+			processing = False
+			return
+		#print("Lunghezza p0a: ", len(uniform_p0a))
+		uniform_tof1 = f.uniform_list_tof(array_support, tof1, min_ts_tof1, max_ts_tof1, min_ts_tof, max_ts_tof)
+		if (not uniform_tof1):
+			processing = False
+			return
+
+		lista_ingressi,lista_uscite = f.get_ground_truth(PATH, DATE, DATA, min_ts, max_ts)
+
+		#print("--- Lato 0")
+		activation_tof0 = f.activate_tof(uniform_tof0)
+		#print("--- Lato 1")
+		activation_tof1 = f.activate_tof(uniform_tof1)
+
+		entrate = 0
+		uscite = 0
+		delta_tof = 1600
+		O=0
+		I=0
+		entrate_act = []
+		uscite_act = []
+		entrate,uscite, entrate_act, uscite_act = f.count_entries_tof(activation_tof0, activation_tof1, delta_tof, I, O, entrate_act,uscite_act)
+		'''
+		if check_matching:
+			with open(OUTPUT_PATH_TOF_TS+"tof_ts.csv", 'a') as match:
+				writer = csv.writer(match, delimiter=';')
+				writer.writerow(['TS', 'TypeOfEvent'])
+				for element in entrate_act:
+					writer.writerow([element + min_ts, 1])
+				for element in uscite_act:
+					writer.writerow([element + min_ts, 0])
+		'''
+		print("------- RILEVAZIONI ----------")
+		print("Entrate ",entrate,"\nUscite ", uscite)
+		if do_graph:
+			plt.figure(1, figsize=(15,8))
+			plt.plot(lista_ingressi, [10]*len(lista_ingressi), 'ro', color='blue', label='GT entries')
+			plt.plot(lista_uscite, [10]*len(lista_uscite), 'ro', color='green', label='GT exits')
+			plt.plot(entrate_act, [100]*len(entrate_act), 'ro', color='blue', label='algorithm entries')
+			plt.plot(uscite_act, [100]*len(uscite_act), 'ro', color='green', label='algorithm exits')
+			plt.plot(uniform_tof1, color='orange', label='Lato 1')
+			plt.plot(uniform_tof0, color='red', label='Lato 0')
+			#plt.legend(handles=[green_tof,red_tof,alg_entries,alg_exits,true_exits,true_entries])
+			plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+
+			plt.show();
+
+	if check_matching:
+		check_in_TOF = []
+		check_out_TOF = []
+		check_in_INF = []
+		check_out_INF = []
+		check_TOF = []
+		check_INF = []
+		for element in entrate_act:
+			check_in_TOF.append((element + min_ts_tof))
+			check_TOF.append(element + min_ts_tof)
+		for element in uscite_act:
+			check_out_TOF.append((element + min_ts_tof))
+			check_TOF.append(element + min_ts_tof)
+		for element in E_inf:
+			check_in_INF.append((element))
+			check_INF.append(element)
+		for element in U_inf:
+			check_out_INF.append(element)
+			check_INF.append(element)
+
+		check_INF.sort()
+		check_TOF.sort()
+		plt.plot(check_in_TOF, [1]*len(check_in_TOF),'ro', color='blue', label='IN TOF')
+		plt.plot(check_out_TOF, [3]*len(check_out_TOF), 'ro', color='blue', label='OUT TOF')
+		plt.plot(check_in_INF, [1.5]*len(check_in_INF), 'rx', color='green', label='IN INF')
+		plt.plot(check_out_INF, [3.5]*len(check_out_INF), 'rx', color='green', label='OUT INF')
+		plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+		plt.show();
+		plt.plot(check_TOF, [1]*len(check_TOF),'ro', color='blue', label='TOF')
+		plt.plot(check_INF, [1.5]*len(check_INF), 'rx', color='green', label='INF')
+		plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+		plt.ylim(0.5,2)
+		plt.show()
+
+
 
 	if use_pir and use_infra:				#opzione non contemplata
 		return I_pir, O_pir, I_inf, O_inf
@@ -281,6 +372,8 @@ def just_processing(a, b, delta, var, use, TIME):
 	elif use_camera:
 		print("Nothing to return, camera data processed.\n")
 		return 1
+	elif use_tof:
+		return entrate, uscite, len(lista_ingressi), len(lista_uscite)
 	elif do_graph:
 		print("Nothing to return, graphs done.\n")
 		return 1
